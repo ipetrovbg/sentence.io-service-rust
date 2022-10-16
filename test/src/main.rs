8803a3764698d4722eaf265d03e83599288b2c77
petarpetrov <ipetrovbg@gmail.com>
2022-10-16 18:19:43 +0300

    feat: bump versions and add logging

--- test/src/main.rs
+++ test/src/main.rs
@@ -1,17 +1,27 @@
 mod models;
 
-use lambda_runtime::Context;
-use lambda_runtime::Error;
+use lambda_runtime::{Error, LambdaEvent, service_fn, run};
 use crate::models::{DynamoDBNewImageTrigger, Output};
+
+use log::{info, LevelFilter};
+use simple_logger::SimpleLogger;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let handler = lambda_runtime::handler_fn(handler);
-    lambda_runtime::run(handler).await?;
+    SimpleLogger::new()
+    .with_level(LevelFilter::Info)
+    .init()
+    .unwrap();
+
+    let handler = service_fn(handler);
+    run(handler).await?;
     Ok(())
 }
 
-async fn handler(event: DynamoDBNewImageTrigger, context: Context) -> Result<Output, Error> {
+async fn handler(event: LambdaEvent<DynamoDBNewImageTrigger>) -> Result<Output, Error> {
+    let (event, context) = event.into_parts();
+    info!("DynamoDB new record: {}", event.dynamodb.new_image);
+
     Ok(Output {
         message: event.dynamodb.new_image,
         request_id: context.request_id,
